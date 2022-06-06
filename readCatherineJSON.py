260fc758a40e5d99201924faca5fdd4f4ffd77a7
cdiproperz <catherine.diproperzio@factset.com>
2022-06-06 11:19:55 -0400

    Added python programs to read 2 json files

---
+++ readCatherineJSON.py
@@ -0,0 +1,12 @@
+import json
+f=open('catherine.json')
+data=json.load(f)
+
+for i in data['catherine']:
+    print(i)
+print("---------------------------------------------------------------")
+f=open('catherine2.json', "r")
+data=json.loads(f.read())
+print(data)
+
+f.close()
