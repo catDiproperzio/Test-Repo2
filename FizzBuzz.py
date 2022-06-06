c7d323a8fabef68b2a446a8637bc35879c8f8c9f
cdiproperz <catherine.diproperzio@factset.com>
2022-06-06 10:35:05 -0400

    Added FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+num=input("Enter Number:")
+num=int( num)
+for i in range(1,num+1):
+    if i%5==0 and i%3==0:
+        print("FizzBuzz")
+    elif i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
